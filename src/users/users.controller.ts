75f05a3665aab385626e9a2c88762740ed3ac3ff
muhamadroushid <muhamadroushid@gmail.com>
2025-06-25 15:01:34 +0530

    order matters

---
+++ src/users/users.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get() //GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get('interns') //GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+}
